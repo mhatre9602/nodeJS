7c8ff682ffca9e667452451223e4c7b5dd4ae2bd
mhatre9602 <pratikk.mhatre93@gmail.com>
2023-10-21 15:22:32 +0530

    Cleanup code

--- redirectingFileFunctions.js
+++ redirectingFileFunctions.js
@@ -1,40 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((request, response) => {
-  if (request.url == "/") {
-    fs.readFile("message.txt", (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      response.write("<html>");
-      response.write("<head><title>Enter Message</title></head>");
-      response.write(`<body><h4>${data}</h4></body`);
-      response.write(
-        "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form></body>"
-      );
-      response.write("</html>");
-      return response.end();
-    });
-  } else if (request.url == "/message" && request.method == "POST") {
-    const body = [];
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
+const routes = require("./routes");
 
-    return request.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        if (err) {
-          console.log(err);
-        }
-        response.statusCode = 302;
-        response.setHeader("Location", "/");
-        return response.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
