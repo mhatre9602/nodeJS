3bb330ad512c9f13c19032acc110cddf96294386
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-10-23 11:35:22 +0530

    body parser code express JS

---
+++ bodyParser.js
@@ -0,0 +1,26 @@
+//express
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded());
+
+app.use("/", (req, res, next) => {
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><input type='text' name='size'><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>ExpressJS</h1>");
+});
+app.listen(3000);
