a3d022a3b8f09fad465309af06a520c97bcf960f
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-10-24 22:14:01 +0530

    EJS and dynamic routing

--- product.js
+++ product.js
@@ -1,31 +1,35 @@
-const { json } = require("body-parser");
 const fs = require("fs");
 const path = require("path");
 
-const pathWrite = path.join(
-  path.dirname(require.main.filename),
+const p = path.join(
+  path.dirname(process.mainModule.filename),
   "data",
   "products.json"
 );
+
 const getProductsFromFile = (cb) => {
-  fs.readFile(pathWrite, (err, fileData) => {
+  fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
     } else {
-      cb(JSON.parse(fileData));
+      cb(JSON.parse(fileContent));
     }
   });
 };
 
-module.exports = class Products {
-  constructor(t) {
-    this.title = t;
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(pathWrite, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -33,5 +37,12 @@ module.exports = class Products {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id == id);
+      cb(product);
+    });
   }
 };
