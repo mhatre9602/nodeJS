5bba10e9250248e3d54108f1f5c908a3eea59e6a
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-10-25 12:12:50 +0530

    Delete functionality as required and edit code
    admin.js controller  1st file
    admin.js routes 2nd files
    and model product.js
    added the delete functionality by taking the id into params and retrieving it and deleting it

--- admin.js
+++ admin.js
@@ -1,12 +1,24 @@
+const path = require("path");
+
 const express = require("express");
 
-const router = express.Router();
-const addProductController = require("../controllers/addProductController");
-router.get("/add-product", addProductController.addProduct);
+const adminController = require("../controllers/admin");
 
-router.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
+
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
+
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product/:productId", adminController.deleteProduct);
 
 module.exports = router;
--- product.js
+++ product.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const Cart = require("./cart");
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   "data",
@@ -18,7 +20,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,11 +29,32 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static deleteProductById(id) {
+    getProductsFromFile((products) => {
+      const updatedProducts = products.filter((product) => product.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+        }
       });
     });
   }
@@ -41,7 +65,7 @@ module.exports = class Product {
 
   static findById(id, cb) {
     getProductsFromFile((products) => {
-      const product = products.find((p) => p.id == id);
+      const product = products.find((p) => p.id === id);
       cb(product);
     });
   }
